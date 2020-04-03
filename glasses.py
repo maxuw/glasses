caeac4bb1862b04f8e352fe103854fcc02c2c127
Max <m.jackl@student.uw.edu.pl>
2020-04-03 02:24:51 +0200

    created a print_content_glasses function that prints the state of glasses filled/limit

--- glasses.py
+++ glasses.py
@@ -1,4 +1,25 @@
-#-
 ## Initial file
+#-
+
+
+
+
+#-
+
+#- print content glasses
+def print_content_glasses(glasses_filled, glasses_limit):
+    return_string = ""
+    ch = 'A'
+
+
+    for i in range(len(glasses_limit)):
+        return_string += ch + ": " + str(glasses_filled[i]) + "/" + str(glasses_limit[i]) + "  "
+        ch= chr(ord(ch) + 1)
+
+    print(return_string)
+#-
+glasses_limit = [1,2]
+glasses_filled = [0,1]
+print_content_glasses(glasses_filled, glasses_limit)
 
 #-
